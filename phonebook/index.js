c05d4cf7110bca84784030fe2ec09eb1a7e9587b
akaVELASQUEZ <allanvelasquez2397@gmail.com>
2022-09-10 20:59:46 +0800

    Exercise 3.3

--- phonebook/index.js
+++ phonebook/index.js
@@ -1,3 +1,4 @@
+const e = require('express')
 const express = require('express')
 const app = express()
 
@@ -37,4 +38,20 @@ app.get('/info', (req, res) => {
     res.send(`Phonebook has info for ${persons.length} people
     <br/>
     ${Date()}`)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(p => p.id === id)
+    console.log(person)
+
+    if (person) {
+        console.log(1)
+        res.json(person)
+    }
+    else {
+        console.log(2)
+        res.status(404).end()
+    }
+    
 })
