aea42d1b5f4e2fc4e9bb22531df520691c40153d
akaVELASQUEZ <allanvelasquez2397@gmail.com>
2022-09-17 23:46:14 +0800

    Exercise 3.14

--- phonebook/index.js
+++ phonebook/index.js
@@ -1,33 +1,15 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
+require('dotenv').config();
 var morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person');
+const { response } = require('express');
+
 
 app.use(express.static('build'))
 app.use(cors())
-
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
 
 app.use(express.json())
 
@@ -46,19 +28,20 @@ app.use(morgan(function (tokens, req, res) {
   })
 )
 
-const generateID = () => {
-    const randomID = Math.floor(Math.random() * 100000)
-    return randomID
-}
+// const generateID = () => {
+//     const randomID = Math.floor(Math.random() * 100000)
+//     return randomID
+// }
 
-
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server Running on port ${PORT}`)
 })
 
 app.get('/api/persons', (req, res) => {
-    res.json(persons)
+    Person.find({}).then(people => {
+        res.json(people)
+      })
 })
 
 app.get('/info', (req, res) => {
@@ -68,15 +51,20 @@ app.get('/info', (req, res) => {
 })
 
 app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const person = persons.find(p => p.id === id)
+    Person.findById(req.params.id)
+        .then(person =>{
+            res.json(person)
+        })
+
+    // const id = Number(req.params.id)
+    // const person = persons.find(p => p.id === id)
     
-    if (person) {
-        res.json(person)
-    }
-    else {
-        res.status(404).end()
-    }
+    // if (person) {
+    //     res.json(person)
+    // }
+    // else {
+    //     res.status(404).end()
+    // }
 })
 
 
@@ -92,28 +80,35 @@ app.delete('/api/persons/:id', (req, res) => {
 app.post('/api/persons', (req, res) => {
     const body = req.body
     
-    const person = {
-        "id": generateID(),
+    // if (body.content === undefined) {
+    //     return res.status(400).json({ error: 'content missing' })
+    // }
+
+    const person = new Person({
         "name": body.name,
         "number": body.number
-    }
+    })
 
-    const nameCheck = persons.find(p => p.name === body.name)
+    person.save().then(savedPerson => {
+        res.json(savedPerson)
+    })
 
-    if (body.name && body.number) {
-        if (nameCheck) {
-            res.status(404).json({
-                error: "Name already exists"
-            })
-        }
-        else {
-            persons = persons.concat(person)
-            res.json(person)
-        }
-    }
-    else {
-        res.status(404).json({
-            error: "Please fill all the fields"
-        })
-    }
+    // const nameCheck = persons.find(p => p.name === body.name)
+
+    // if (body.name && body.number) {
+    //     if (nameCheck) {
+    //         res.status(404).json({
+    //             error: "Name already exists"
+    //         })
+    //     }
+    //     else {
+    //         persons = persons.concat(person)
+    //         res.json(person)
+    //     }
+    // }
+    // else {
+    //     res.status(404).json({
+    //         error: "Please fill all the fields"
+    //     })
+    // }        
 })
