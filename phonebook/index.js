ecf2be39132ea8142e6d3b99a901d464aebd3a1f
akaVELASQUEZ <allanvelasquez2397@gmail.com>
2022-09-18 11:21:29 +0800

    Exercise 3.19

--- phonebook/index.js
+++ phonebook/index.js
@@ -86,25 +86,34 @@ app.put('/api/persons/:id', (req, res, next) => {
 })
 
 
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
     const body = req.body
+
     const person = new Person({
         "name": body.name,
         "number": body.number
     })
 
     person.save().then(savedPerson => {
-        res.json(savedPerson)
-    })     
+        res.json(savedPerson.toJSON())
+    }) 
+    .catch(error => next(error))    
 })
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+  
+app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
     console.error(error.message)
   
     if (error.name === 'CastError') {
-      return response.status(400).send({ error: 'malformatted id' })
-    } 
-  
+        return response.status(400).send({ error: 'malformatted id' })
+    }  else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+    }
     next(error)
 }
 
